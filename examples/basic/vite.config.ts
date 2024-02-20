fe0d40b4e4c508f590cc6d999aa64f8e9eace06a
Noah Wilson <noah@gavel.io>
2024-02-20 15:50:33 -0800

    hanging repro

--- examples/basic/vite.config.ts
+++ examples/basic/vite.config.ts
@@ -2,11 +2,43 @@
 
 // Configure Vitest (https://vitest.dev/config/)
 
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import swc from 'unplugin-swc'
+import { defineConfig, mergeConfig } from 'vite'
 
-export default defineConfig({
+const sharedConfig = defineConfig({
   test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
-    // globals: true,
+    environment: 'node',
+    globals: true,
+    restoreMocks: true,
+    testTimeout: 5000,
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@test': path.resolve(__dirname, './test'),
+    },
   },
+  plugins: [
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
 })
+
+const unitTests = mergeConfig(
+  sharedConfig,
+  defineConfig({
+    test: {
+      name: 'unit',
+      dir: path.resolve(__dirname, './test'),
+      include: ['**/*.test.ts'],
+      // globalSetup: ['./test/setupTests.ts'],
+      reporters: ['default', 'junit'],
+      outputFile: path.resolve(__dirname, './coverage/unit/junit.xml'),
+      coverage: { enabled: true },
+      restoreMocks: true,
+    },
+  }),
+)
+
+export default unitTests
